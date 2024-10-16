bda419a7596c6c831c2619d2db5b24aa3679362f
Vincent Horecký <vincent.horecky@seznam.cz>
2024-10-16 15:58:00 +0200

    removed old timers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,35 +8,16 @@
 <main>
   <Timer
     timerType="Entire-class"
-    wantedTime="2024-10-15T10:55:32"
-    name="Budeš zničena názvoslovím, S2<br />S1, budeš zničena kalorimetrickou rovnicí."
-    description="Test z chemie (S2), test z fyziky (S1)"
-  />
-  <Timer
-    timerType="Entire-class"
-    wantedTime="2024-10-15T11:50"
-    name="Budeš zničena názvoslovím, S1.<br />S2, budeš zničena kalorimetrickou rovnicí."
-    description="Test z chemie (S1), test z fyziky (S2)"
-  />
-  <Timer
-    timerType="French"
-    wantedTime="2024-10-16T10:56:43"
-    name="Test for the French!"
-    description="Test z francouzštiny."
-  />
-  <Timer
-    timerType="Entire-class"
     wantedTime="2024-10-18T08:56:04"
     name="nezapomeň na PODMÍNKY!!!"
     description="Test z matematiky."
   />
-  <!-- <Timer
+  <Timer
   timerType="French"
-  wantedTime={"2024-10-23T10:55:27"}
+  wantedTime="2024-10-23T10:55:27"
   name="Another wednesday, another test for the French!"
   description="Test z francouzštiny (zase)"
-  /> -->
-  <!-- ↑ Add on 24/10 ↑ -->
+  />
   <Timer
     timerType="S1"
     wantedTime="2024-10-21T08:01:11"
