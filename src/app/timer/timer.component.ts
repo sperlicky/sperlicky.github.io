73253a3d4f2ece7871d7b94a2ce8828719031d15
Vincent Horecký <vincent.horecky@seznam.cz>
2024-11-14 18:06:44 +0100

    just refactoring, removing old timers remove old timers = ROT

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,66 +1,78 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import {Component, Input, OnInit, OnDestroy} from '@angular/core';
 import gsap from 'gsap';
-import { DecimalPipe } from '@angular/common';
+import {DecimalPipe} from '@angular/common';
 
 @Component({
-  selector: 'app-timer',
-  standalone: true,
-  imports: [DecimalPipe],
-  templateUrl: './timer.component.html',
-  styleUrl: './timer.component.scss',
+    selector: 'app-timer',
+    standalone: true,
+    imports: [DecimalPipe],
+    templateUrl: './timer.component.html',
+    styleUrl: './timer.component.scss',
 })
 export class TimerComponent implements OnInit, OnDestroy {
-  @Input({ required: true, alias: 'type' }) timerType: TimerType =
-    'Entire-class';
-  @Input({ required: true }) name = '';
-  @Input({ required: true }) description = '';
-  @Input({ required: true, alias: 'time' }) wantedTime = '';
+    @Input({required: true, alias: 'type'}) timerType: TimerType =
+        'Entire-class';
+    @Input({required: true}) name = '';
+    @Input({required: true}) description = '';
+    @Input({required: true, alias: 'time'}) wantedTime = '';
 
-  theTime!: Date;
-  currentTime = new Date();
-  interval!: ReturnType<typeof setInterval>;
-  shouldBeHidden = false;
+    theTime!: Date;
+    currentTime = new Date();
+    interval!: ReturnType<typeof setInterval>;
+    shouldBeHidden = false;
 
-  msDay = 24 * 60 * 60 * 1000;
-  msHour = 60 * 60 * 1000;
-  msMinute = 60 * 1000;
-  msSecond = 1000;
+    msDay = 24 * 60 * 60 * 1000;
+    msHour = 60 * 60 * 1000;
+    msMinute = 60 * 1000;
+    msSecond = 1000;
 
-  timeValues = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    timeValues = {days: 0, hours: 0, minutes: 0, seconds: 0};
 
-  updateTime = () => {
-    this.currentTime = new Date();
-    const timeDiff = this.theTime.getTime() - this.currentTime.getTime();
-    if (timeDiff <= 0) {
-      clearInterval(this.interval);
-      this.timeValues = { days: 0, hours: 0, minutes: 0, seconds: 0 };
-      this.shouldBeHidden = true;
-      return;
+    updateTime = () => {
+        this.currentTime = new Date();
+        const timeDiff = this.theTime.getTime() - this.currentTime.getTime();
+        if (timeDiff <= 0) {
+            clearInterval(this.interval);
+            this.timeValues = {days: 0, hours: 0, minutes: 0, seconds: 0};
+            this.shouldBeHidden = true;
+            return;
+        }
+        this.updateTimeVariables(timeDiff);
+    };
+
+    private updateTimeVariables = (timeDiff: number) => {
+        gsap.to(this.timeValues, {
+            seconds: Math.floor((timeDiff % this.msMinute) / this.msSecond),
+            minutes: Math.floor((timeDiff % this.msHour) / this.msMinute),
+            hours: Math.floor((timeDiff % this.msDay) / this.msHour),
+            days: Math.floor(timeDiff / this.msDay),
+            duration: 0.2,
+            ease: 'power2.out',
+        });
     }
 
-    gsap.to(this.timeValues, {
-      seconds: Math.floor((timeDiff % this.msMinute) / this.msSecond),
-      minutes: Math.floor((timeDiff % this.msHour) / this.msMinute),
-      hours: Math.floor((timeDiff % this.msDay) / this.msHour),
-      days: Math.floor(timeDiff / this.msDay),
-      ease: 'power2.out',
-    });
-  };
-  ngOnInit(): void {
-    this.interval = setInterval(this.updateTime, 1000);
-    console.log(this.wantedTime);
-    console.log(this.theTime);
-    this.theTime = new Date(this.wantedTime);
-  }
-  ngOnDestroy(): void {
-    clearInterval(this.interval);
-  }
+    ngOnInit(): void {
+        this.theTime = new Date(this.wantedTime);
+        const timeDiff = this.theTime.getTime() - this.currentTime.getTime();
+        if (timeDiff <= 0) {
+            this.shouldBeHidden = true;
+            this.timeValues = {days: 0, hours: 0, minutes: 0, seconds: 0};
+        } else {
+            this.currentTime = new Date();
+            this.updateTimeVariables(timeDiff);
+            this.interval = setInterval(this.updateTime, 1000);
+        }
+    }
+
+    ngOnDestroy(): void {
+        clearInterval(this.interval);
+    }
 }
 
 type TimerType =
-  | 'Entire-class'
-  | 'S1'
-  | 'S2'
-  | 'French'
-  | 'German'
-  | 'Positive';
+    | 'Entire-class'
+    | 'S1'
+    | 'S2'
+    | 'French'
+    | 'German'
+    | 'Positive';
