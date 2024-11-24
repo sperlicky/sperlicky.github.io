c01c3f01e00df8191eff237c009ec04a141cfa7f
Vincent Horecký <vincent.horecky@seznam.cz>
2024-11-24 13:55:01 +0100

    timezones!

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,99 +1,115 @@
-import {Component, Input, OnInit, OnDestroy} from '@angular/core';
-import {DecimalPipe} from '@angular/common';
-import {animate, style, transition, trigger} from "@angular/animations";
+import { Component, OnInit, OnDestroy, input } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { DateTime, Duration } from 'luxon';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
-    selector: 'app-timer',
-    imports: [DecimalPipe],
-    templateUrl: './timer.component.html',
-    styleUrl: './timer.component.scss',
-    animations: [
-        trigger('flipAnimation', [
-            transition(':increment', [
-                animate("0.2s ease-out", style({ transform: "translateY(0.5em)", opacity: 0 })),
-                style({ transform: 'translateY(-1em)' }),
-                animate('0.2s ease-out', style({ transform: 'translateY(0)', opacity: 1 })),
-            ]),
-            transition(':decrement', [
-                animate("0.2s ease-out", style({ transform: "translateY(0.5em)", opacity: 0 })),
-                style({ transform: 'translateY(-1em)' }),
-                animate('0.2s ease-out', style({ transform: 'translateY(0)', opacity: 1 })),
-            ]),
-        ]),
-    ]
+  selector: 'app-timer',
+  imports: [DecimalPipe],
+  templateUrl: './timer.component.html',
+  styleUrl: './timer.component.scss',
+  animations: [
+    trigger('flipAnimation', [
+      transition(':increment', [
+        animate(
+          '0.2s ease-out',
+          style({ transform: 'translateY(0.5em)', opacity: 0 }),
+        ),
+        style({ transform: 'translateY(-1em)' }),
+        animate(
+          '0.2s ease-out',
+          style({ transform: 'translateY(0)', opacity: 1 }),
+        ),
+      ]),
+      transition(':decrement', [
+        animate(
+          '0.2s ease-out',
+          style({ transform: 'translateY(0.5em)', opacity: 0 }),
+        ),
+        style({ transform: 'translateY(-1em)' }),
+        animate(
+          '0.2s ease-out',
+          style({ transform: 'translateY(0)', opacity: 1 }),
+        ),
+      ]),
+    ]),
+  ],
 })
 export class TimerComponent implements OnInit, OnDestroy {
-    @Input({required: true, alias: 'type'}) timerType: TimerType =
-        'Entire-class';
-    @Input({required: true}) name = '';
-    @Input({required: true}) description = '';
-    @Input({required: true, alias: 'time'}) wantedTime = '';
+  timerType = input.required<TimerType>({ alias: 'type' });
+  name = input.required();
+  description = input.required();
+  wantedTime = input.required<string>({ alias: 'time' });
 
-    theTime!: Date;
-    currentTime = new Date();
-    interval!: ReturnType<typeof setInterval>;
-    shouldBeHidden = false;
+  theTime!: DateTime;
+  currentTime = DateTime.now().setZone('Europe/Prague');
+  interval!: ReturnType<typeof setInterval>;
+  shouldBeHidden = false;
 
-    msDay = 24 * 60 * 60 * 1000;
-    msHour = 60 * 60 * 1000;
-    msMinute = 60 * 1000;
-    msSecond = 1000;
+  technicalTimeValues = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+  timeValues = { days: 0, hours: 0, minutes: 0, seconds: 0 };
 
-    technicalTimeValues = {days: 0, hours: 0, minutes: 0, seconds: 0};
-    timeValues = {days: 0, hours: 0, minutes: 0, seconds: 0}
+  updateTime = () => {
+    this.currentTime = DateTime.now().setZone('Europe/Prague');
+    console.log(this.currentTime);
+    const eventIn = Duration.fromMillis(
+      this.theTime.toMillis() - this.currentTime.toMillis(),
+    );
+    if (eventIn.as('millisecond') <= 0) {
+      clearInterval(this.interval);
+      this.technicalTimeValues = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+      this.shouldBeHidden = true;
+      return;
+    }
+    this.updateTimeVariables(eventIn);
+  };
 
-
-    updateTime = () => {
-        this.currentTime = new Date();
-        const timeDiff = this.theTime.getTime() - this.currentTime.getTime();
-        if (timeDiff <= 0) {
-            clearInterval(this.interval);
-            this.technicalTimeValues = {days: 0, hours: 0, minutes: 0, seconds: 0};
-            this.shouldBeHidden = true;
-            return;
-        }
-        this.updateTimeVariables(timeDiff);
+  private updateTimeVariables = (timeDiff: Duration) => {
+    const technicalTimeDiff = timeDiff
+      .shiftTo('days', 'hours', 'minutes', 'seconds')
+      .toObject();
+    this.technicalTimeValues = {
+      seconds: technicalTimeDiff.seconds || 0,
+      minutes: technicalTimeDiff.minutes || 0,
+      hours: technicalTimeDiff.hours || 0,
+      days: technicalTimeDiff.days || 0,
     };
+    setTimeout(() => {
+      this.timeValues = {
+        seconds: technicalTimeDiff.seconds || 0,
+        minutes: technicalTimeDiff.minutes || 0,
+        hours: technicalTimeDiff.hours || 0,
+        days: technicalTimeDiff.days || 0,
+      };
+    }, 200);
+  };
 
-    private updateTimeVariables = (timeDiff: number) => {
-        this.technicalTimeValues = {
-            seconds: Math.floor((timeDiff % this.msMinute) / this.msSecond),
-            minutes: Math.floor((timeDiff % this.msHour) / this.msMinute),
-            hours: Math.floor((timeDiff % this.msDay) / this.msHour),
-            days: Math.floor(timeDiff / this.msDay),
-        }
-        setTimeout(() => {
-            this.timeValues = {
-                seconds: Math.floor((timeDiff % this.msMinute) / this.msSecond),
-                minutes: Math.floor((timeDiff % this.msHour) / this.msMinute),
-                hours: Math.floor((timeDiff % this.msDay) / this.msHour),
-                days: Math.floor(timeDiff / this.msDay),
-            }
-        }, 200)
+  ngOnInit(): void {
+    this.theTime = DateTime.fromISO(this.wantedTime(), {
+      zone: 'Europe/Prague',
+    });
+    const eventIn = Duration.fromMillis(
+      this.theTime.toMillis() - this.currentTime.toMillis(),
+    );
+    if (eventIn.as('milliseconds') <= 0) {
+      this.shouldBeHidden = true;
+      this.timeValues = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    } else {
+      this.currentTime = DateTime.now().setZone('Europe/Prague');
+      setTimeout(() => this.updateTimeVariables(eventIn), 100);
+      this.interval = setInterval(this.updateTime, 1000);
     }
+  }
 
-    ngOnInit(): void {
-        this.theTime = new Date(this.wantedTime);
-        const timeDiff = this.theTime.getTime() - this.currentTime.getTime();
-        if (timeDiff <= 0) {
-            this.shouldBeHidden = true;
-            this.timeValues = {days: 0, hours: 0, minutes: 0, seconds: 0};
-        } else {
-            this.currentTime = new Date();
-            setTimeout(() => this.updateTimeVariables(timeDiff), 100)
-            this.interval = setInterval(this.updateTime, 1000);
-        }
-    }
-
-    ngOnDestroy(): void {
-        clearInterval(this.interval);
-    }
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
 }
 
 type TimerType =
-    | 'Entire-class'
-    | 'S1'
-    | 'S2'
-    | 'French'
-    | 'German'
-    | 'Positive';
+  | 'Entire-class'
+  | 'S1'
+  | 'S2'
+  | 'French'
+  | 'German'
+  | 'Positive';
